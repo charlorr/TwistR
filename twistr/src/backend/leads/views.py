36dccfabebec3e3f39decd5cc2d8fa51d740ca85
lewa1000g <aesonakhras@gmail.com>
2019-09-18 15:07:24 -0400

    yeet

---
+++ twistr/src/backend/leads/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from leads.models import Lead
+from leads.serializers import LeadSerializer
+from rest_framework import generics
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
