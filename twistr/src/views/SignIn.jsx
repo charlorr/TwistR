ff042d73d35873eb261fe67ad24435e6f8b09ad9
Ania Szeszko <aszeszko@purdue.edu>
2019-09-27 14:09:37 -0400

    log in updated

--- twistr/src/views/SignIn.jsx
+++ twistr/src/views/SignIn.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col
+} from "reactstrap";
+
 class SignIn extends React.Component {
   render() {
     return (
-      
-      <div>
-        <form method="post">
-          
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder = "Enter Username" name ="uname" required></input>
-          <label for ="pass"><b>Password</b></label>
-          <input type ="password" placeholder = "Enter Password" name ="pass" required></input>
-          <button type="submit">Login</button>
-
-        </form>
-      </div>
-       
+      <Card className="card-user">
+                <CardHeader>
+                  <CardTitle tag="h5">Log In</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Form>
+                        <FormGroup>
+                          <label><b>Username</b></label>
+                          <input placeholder="Enter Username" type="text"/>
+                        </FormGroup>
+                        <FormGroup>
+                          <label><b>Password</b></label>
+                          <input placeholder="Enter Password"  type="password" />
+                        </FormGroup>
+                      <div className="update ml-auto mr-auto">
+                        <Button className="btn-round" color="primary" type="submit"> Log In </Button>
+                      </div>
+                  </Form>
+                </CardBody>
+              </Card>
     );
   }
 }
