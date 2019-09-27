5385d27331691cd0a082e65276bce964dd9e1acb
Ania Szeszko <aszeszko@purdue.edu>
2019-09-27 16:03:59 -0400

    added registration UI

--- twistr/src/views/SignIn.jsx
+++ twistr/src/views/SignIn.jsx
@@ -17,27 +17,82 @@ import {
 class SignIn extends React.Component {
   render() {
     return (
-      <div className="content">
+      <div className="content" >
         <Card className="card-user">
-                  <CardHeader>
+                  <CardHeader className ="update ml-auto mr-auto">
                     <CardTitle tag="h5">Log In</CardTitle>
                   </CardHeader>
-                  <CardBody>
+                  <CardBody className ="update ml-auto mr-auto">
                     <Form>
-                          <FormGroup>
+                      <Row>
+                        <FormGroup>
                             <label><b>Username</b></label>
-                            <Input placeholder="Enter Username" type="text"/>
+                            <Input name = "username" placeholder="Enter Username" type="text"/>
                           </FormGroup>
+                          </Row>
+                          <Row>
                           <FormGroup>
                             <label><b>Password</b></label>
-                            <Input placeholder="Enter Password"  type="password" />
+                            <Input name = "password" placeholder="Enter Password"  type="password" />
                           </FormGroup>
-                        <div className="update ml-auto mr-auto">
-                          <Button className="btn-round" size = "lg" color="primary" type="submit"> Log In </Button>
-                        </div>
+                          </Row>
+                          <Row>
+                            <div className="update ml-auto mr-auto">
+                              <Button className="btn-round" size = "lg" color="primary" type="submit"> Log In </Button>
+                              </div>
+                         </Row>
                     </Form>
                   </CardBody>
           </Card>
+
+          <Card className="card-user">
+                <CardHeader>
+                  <CardTitle tag="h5">Register</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Form>
+                    <Row>
+                      <Col className="pr-1" md="5">
+                        <FormGroup>
+                          <label><b>Username</b></label>
+                          <Input name ="username" placeholder="Enter Username" type="text" required />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="4">
+                        <FormGroup>
+                          <label> <b>Email Address</b> </label>
+                          <Input name = "email" placeholder="Email" type="email" pattern = "/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/" required/>
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="3">
+                        <FormGroup>
+                          <label><b>Phone Number</b></label>
+                          <Input type="tel" name="phone" placeholder ="Enter Phone Number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="6">
+                        <FormGroup>
+                          <label><b>First Name</b></label>
+                          <Input placeholder="Enter First Name" type="text" required />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="6">
+                        <FormGroup>
+                          <label><b>Last Name</b></label>
+                          <Input placeholder="Enter Last Name" type="text" required />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div className="update ml-auto mr-auto">
+                        <Button className="btn-round" size="lg" color="primary" type="submit"> Register </Button>
+                      </div>
+                    </Row>
+                  </Form>
+                </CardBody>
+              </Card>
         </div>
     );
   }
