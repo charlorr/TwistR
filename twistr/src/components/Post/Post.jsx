79115ca7d08addf583e62ae49a7e74378c46a6bd
ckishore <ckishore@purdue.edu>
2019-10-02 16:28:15 -0400

    add UserCard component and hardcoded data

--- twistr/src/components/Post/Post.jsx
+++ twistr/src/components/Post/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCard from "components/UserCard/UserCard.jsx";
 
 import {
   Card,
@@ -6,43 +7,34 @@ import {
   CardBody,
   CardFooter,
   CardTitle,
-  Col
+  Col,
+  Row
 } from "reactstrap";
 
 class Post extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      author: "Ania Szesko",
-      tags: [
-        "IE", "salad", "Purdue"
-      ],
-      content: "I like salad",
-      timestamp: 8,
-    };
-  }
-
   render() {
     return (
       <>
-      <Col md="9">
-        <Card>
-          <CardHeader>
-            <CardTitle tag="h5">{this.state.author}</CardTitle>
-            <p className="card-category">{this.state.tags}</p>
-          </CardHeader>
-          <CardBody>
-            <h1>{this.state.content}</h1>
-          </CardBody>
-          <CardFooter>
-            <hr />
-            <div className="stats">
-              <i className="fa fa-history" /> Updated {this.state.timestamp} minutes ago
-            </div>
-          </CardFooter>
-        </Card>
-      </Col>
+      <Row>
+        <UserCard userPic={this.props.post.picture}/>
+        <Col lg="8" md="6" sm="6">
+          <Card>
+            <CardHeader>
+              <CardTitle tag="h5">{this.props.post.author}</CardTitle>
+              <p className="card-category">{this.props.post.tags}</p>
+            </CardHeader>
+            <CardBody>
+              <h1>{this.props.post.content}</h1>
+            </CardBody>
+            <CardFooter>
+              <hr />
+              <div className="stats">
+                <i className="fa fa-history" /> Updated {this.props.post.timestamp} minutes ago
+              </div>
+            </CardFooter>
+          </Card>
+        </Col>
+      </Row>
       </>
     );
   }
--- twistr/src/components/PostRoster/PostRoster.jsx
+++ twistr/src/components/PostRoster/PostRoster.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Post from "components/Post/Post.jsx";
 
 import {
   Card,
@@ -9,45 +10,6 @@ import {
   Col,
   Row
 } from "reactstrap";
-
-class Post extends React.Component {
-  render() {
-    return (
-      <>
-      <Row>
-        <Col lg="2" md="2" sm="1">
-          <Card>
-            <CardBody>
-              <img
-                alt="..."
-                className="border-gray"
-                src={require("assets/img/PurduePete.jpg")}
-              />
-            </CardBody>
-          </Card>
-        </Col>
-        <Col lg="8" md="6" sm="6">
-          <Card>
-            <CardHeader>
-              <CardTitle tag="h5">{this.props.post.author}</CardTitle>
-              <p className="card-category">{this.props.post.tags}</p>
-            </CardHeader>
-            <CardBody>
-              <h1>{this.props.post.content}</h1>
-            </CardBody>
-            <CardFooter>
-              <hr />
-              <div className="stats">
-                <i className="fa fa-history" /> Updated {this.props.post.timestamp} minutes ago
-              </div>
-            </CardFooter>
-          </Card>
-        </Col>
-      </Row>
-      </>
-    );
-  }
-}
 
 class PostRoster extends React.Component {
   render() {
