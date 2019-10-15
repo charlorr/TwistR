4c03768b55c6f1c0cd82b688dce6fc2b460230cb
Colin Samuels <samuelc@purdue.edu>
2019-10-15 14:05:56 -0400

    Required fields register/profile

--- twistr/src/views/SignIn.jsx
+++ twistr/src/views/SignIn.jsx
@@ -25,8 +25,8 @@ class SignIn extends React.Component {
                     <Form >{/*method="post"*/}
                       <Row>
                         <FormGroup>
-                            <label><b>Username</b></label>
-                            <Input name = "username" placeholder="Enter Username" type="text"/>
+                            <label><b>Username/Email</b></label>
+                            <Input name = "username" placeholder="Enter Username/Email" type="text"/>
                           </FormGroup>
                           </Row>
                           <Row>
@@ -47,46 +47,67 @@ class SignIn extends React.Component {
           <Card className="card-user">
                 <CardHeader>
                   <CardTitle tag="h5">Register</CardTitle>
+                  <p><font color="red">*</font>Required</p>
                 </CardHeader>
                 <CardBody>
                   <Form>
                     <Row>
                       <Col className="pr-1" md="5">
                         <FormGroup>
-                          <label><b>Username</b></label>
+                          <label><b>Username<font color="red">*</font></b></label>
                           <Input name ="username" placeholder="Enter Username" type="text" required />
                         </FormGroup>
                       </Col>
                       <Col className="pl-1" md="4">
                         <FormGroup>
-                          <label> <b>Email Address</b> </label>
-                          <Input name = "email" placeholder="Email" type="email" required/>
+                          <label> <b>Email Address<font color="red">*</font></b> </label>
+                          <Input name = "email" placeholder="Email" type="example@twistr.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{1,63}$" required/>
                         </FormGroup>
                       </Col>
                       <Col className="pl-1" md="3">
                         <FormGroup>
-                          <label><b>Phone Number</b></label>
-                          <Input type="tel" name="phone" placeholder ="Enter Phone Number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
+                          <label><b>Phone Number<font color="red">*</font></b></label>
+                          <Input type="tel" name="phone" placeholder ="Enter Phone Number" pattern="[0-9]{3}[-]?[0-9]{3}[-]?[0-9]{4}"/>
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
                       <Col className="pr-1" md="6">
                         <FormGroup>
-                          <label><b>First Name</b></label>
+                          <label><b>First Name<font color="red">*</font></b></label>
                           <Input placeholder="Enter First Name" type="text" required />
                         </FormGroup>
                       </Col>
                       <Col className="pl-1" md="6">
                         <FormGroup>
-                          <label><b>Last Name</b></label>
+                          <label><b>Last Name<font color="red">*</font></b></label>
                           <Input placeholder="Enter Last Name" type="text" required />
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
+                      <Col className="pr-1" md="6">
+                        <FormGroup>
+                          <label><b>Password<font color="red">*</font></b></label>
+                          <Input placeholder="Password" type="text" required />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="6">
+                        <FormGroup>
+                          <label><b>Confirm Password<font color="red">*</font></b></label>
+                          <Input placeholder="Password" type="text" required />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
                       <div className="update ml-auto mr-auto">
-                        <Button className="btn-round" size="lg" color="primary" type="submit"> Register </Button>
+                        <Button 
+                          className="btn-round" 
+                          size="lg" 
+                          color="primary" 
+                          type="submit"> 
+                          
+                          Register </Button>
                       </div>
                     </Row>
                   </Form>
