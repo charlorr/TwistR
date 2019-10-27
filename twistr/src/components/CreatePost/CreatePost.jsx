a3fb01d29e62ff7cc72f70c82f060d30d573a367
Kaitlyn <kaitlyn@10.17.72.18>
2019-10-27 13:48:33 -0400

    added backend connection to CreatePost

--- twistr/src/components/CreatePost/CreatePost.jsx
+++ twistr/src/components/CreatePost/CreatePost.jsx
@@ -1,25 +1,75 @@
 import React from 'react';
 import UserCard from "components/UserCard/UserCard.jsx";
 import NameCard from "components/NameCard/NameCard.jsx";
+import PostService from "components/PostService/PostService.jsx";
+import NotificationAlert from "react-notification-alert";
 
 import {
+  Button,
   Card,
   CardBody,
   CardFooter,
   CardTitle,
+  Form,
   Col,
   Input,
   FormGroup,
   Row
 } from "reactstrap";
 
+const postService = new PostService();
+
 class CreatePost extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      posts: [],
+      currentPostPk: null,
+      currentPost: [],
       chars_left: 280, max_chars: 280,
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    const current = postService.getPost(this.props.currentPost);
+    console.log(current);
+  }
+
+  notificationAlert = React.createRef();
+  handleSubmit(event) {
+    this.updatePost(this.props.currentPost.pk)
+    event.preventDefault();
+  }
+
+  updatePost(pk) {
+    var author = document.getElementById("author").value;
+    var text = document.getElementById("text").value;
+
+    postService.updatePost({
+      "pk": pk,
+      "author": author,
+      "text": text
+    })
+    .then((result) => {
+      console.log(result);
+      var options = {};
+      options = {
+        place: "tr",
+        message: (
+          <div>
+            <div>
+              Post successfully updated!
+            </div>
+          </div>
+        ),
+        type: "warning",
+        icon: "nc-icon nc-bell-55",
+        autoDismiss: 7
+      };
+      this.notificationAlert.current.notificationAlert(options);
+    })
+    .catch(()=>{
+      alert('There was an error! Please re-check your form.');
+    });
   }
 
   handleWordCount = event => {
@@ -34,13 +84,19 @@ class CreatePost extends React.Component {
       <>
       <Col lg="9" md="6" sm="6">
         <Card className="card-stats">
+          <NotificationAlert ref ={this.notificationAlert} />
           <CardBody>
+            <Form onSubmit={this.handleSubmit}>
+              {
+            }<div>
             <Row>
               <Col lg="8" md="8">
                 <CardTitle tag="h5">Write a new post here.</CardTitle>
                 <FormGroup>
                     <label>Be creative! Remember to use at least one tag.</label>
                     <Input
+                        id="text"
+                        placeholder="Write your post here!"
                         type="textarea"
                         maxLength="280"
                         required
@@ -55,6 +111,19 @@ class CreatePost extends React.Component {
                 </FormGroup>
               </Col>
             </Row>
+            <Row>
+              <div className="update ml-auto mr-auto">
+                <Button
+                className="btn-round"
+                color="primary"
+                type="submit"
+                >
+                  Update Post
+                </Button>
+              </div>
+            </Row>
+            </div>
+            </Form>
           </CardBody>
           <CardFooter>
             <hr />
