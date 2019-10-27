87779a79a24a01a125e31826f15bad0aedb1f31f
Kaitlyn <kaitlyn@10.17.72.18>
2019-10-27 13:19:15 -0400

    post + tag prelim frontend

---
+++ twistr/src/components/PostList/PostList.jsx
@@ -0,0 +1,75 @@
+import  React, { Component } from  'react';
+import  PostService  from  'components/PostService/PostService.jsx';
+
+const  postService  =  new  PostService();
+
+class  PostList  extends  Component {
+
+constructor(props) {
+    super(props);
+    this.state  = {
+        users: [],
+        nextPageURL:  ''
+    };
+    this.nextPage  =  this.nextPage.bind(this);
+    this.handleDelete  =  this.handleDelete.bind(this);
+}
+
+componentDidMount() {
+    var  self  =  this;
+    postService.getPosts().then(function (result) {
+        console.log(result);
+        self.setState({ posts:  result.data, nextPageURL:  result.nextlink})
+    });
+}
+handleDelete(e,pk){
+    var  self  =  this;
+    postService.deletePost({pk :  pk}).then(()=>{
+        var  newArr  =  self.state.users.filter(function(obj) {
+            return  obj.pk  !==  pk;
+        });
+
+        self.setState({posts:  newArr})
+    });
+}
+
+nextPage(){
+    var  self  =  this;
+    console.log(this.state.nextPageURL);        
+    postService.getPostsByURL(this.state.nextPageURL).then((result) => {
+        self.setState({ posts:  result.data, nextPageURL:  result.nextlink})
+    });
+}
+render() {
+
+    return (
+        <div  className="posts--list">
+            <table  className="table">
+            <thead  key="thead">
+            <tr>
+                <th>#</th>
+                <th>Author</th>
+                <th>Text</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.users.map( c  =>
+                <tr  key={c.pk}>
+                <td>{c.pk}  </td>
+                <td>{c.author}</td>
+                <td>{c.text}</td>
+                
+                <td>
+                <button  onClick={(e)=>  this.handleDelete(e,c.pk) }> Delete</button>
+                <a  href={"post/" + c.pk}> Update</a>
+                </td>
+            </tr>)}
+            </tbody>
+            </table>
+            <button  className="btn btn-primary"  onClick=  {  this.nextPage  }>Next</button>
+        </div>
+        );
+  }
+}
+export  default  UserList;
---
+++ twistr/src/components/PostService/PostService.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000';
+
+export default class PostService{
+
+    getPosts() {
+        const url = `${API_URL}/api/posts/`;
+        return axios.get(url).then(response => response.data);
+    }  
+    getPostsByURL(link){
+        const url = `${API_URL}${link}`;
+        return axios.get(url).then(response => response.data);
+    }
+    getPost(pk) {
+        const url = `${API_URL}/api/posts/${pk}`;
+        return axios.get(url).then(response => response.data);
+    }
+    deletePost(post){
+        const url = `${API_URL}/api/posts/${post.pk}`;
+        return axios.delete(url);
+    }
+    createPost(post){
+        const url = `${API_URL}/api/posts/`;
+        return axios.post(url,post);
+    }
+    updatePost(post){
+        const url = `${API_URL}/api/posts/${post.pk}`;
+        return axios.put(url,post);
+    }
+}
