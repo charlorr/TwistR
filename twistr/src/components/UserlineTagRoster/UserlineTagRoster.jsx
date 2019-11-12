86f0ee7b5d640c97b9e7884807de408faec52abb
Ania Szeszko <aszeszko@purdue.edu>
2019-11-12 15:29:04 -0500

    twist roster on userline udpates

--- twistr/src/components/UserlineTagRoster/UserlineTagRoster.jsx
+++ twistr/src/components/UserlineTagRoster/UserlineTagRoster.jsx
@@ -8,14 +8,29 @@ import {
 } from "reactstrap";
 
 class UserlineTagRoster extends React.Component {
+
+  //conditionally render whether tag is within list of followed tags. if yes, render button as primary color.
+  //else render it as secondary button color
   render() {
     // Create tags
+
+    // let tagButton;
+    // if (){
+    //   tagButton = <Button className = "btn-round" size="lg" color="primary"/>
+    // }
+    // else{
+    //   tagButton = <Button className ="btn-round" size="lg" color="secondary"/>
+    // }
+		
     var cards = [];
-    this.props.tags.forEach(function(tag) { 
+    this.props.tags_all.forEach(function(tag) { //currently displaying all tags regardless of follow or not
         cards.push(
           <Col lg="3" md="3" sm="3">
-            <Button>
-                {tag.name}
+            <Button
+            className="btn-round" 
+            size="lg" 
+            color="primary">
+                {tag.pk}
             </Button>
           </Col>
         );
